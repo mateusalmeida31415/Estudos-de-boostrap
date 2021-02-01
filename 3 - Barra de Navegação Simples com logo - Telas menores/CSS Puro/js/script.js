24e69fd58436c6812d19c3e504ec84695f34a60a
Mateus Almeida <mateus.almeida31415@gmail.com>
2021-02-01 12:10:16 -0300

    Barra de navegação responsiva com CSS e JS

---
+++ 3 - Barra de Navegação Simples com logo - Telas menores/CSS Puro/js/script.js
@@ -0,0 +1,5 @@
+let btn = document.getElementsByClassName('navbar-toggler-icon')
+let hiddenMenu = document.getElementsByClassName('navbar')
+btn.addEventListener('click', () =>{
+    document.hiddenMenu.style.background = '#ffffff'
+})
